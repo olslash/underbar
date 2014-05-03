87f9b0e026cebf3ae9172bef3fc7a1fcbbfdf45b
Mitch Robb <olordhux+github@gmail.com>
2014-05-02 22:28:03 -0700

    finished .last

--- src/underbar.js
+++ src/underbar.js
@@ -39,6 +39,10 @@ var _ = {};
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    if (n===0)
+      return [];
+    
+    return n === undefined ? array[array.length-1] : array.slice(-n, array.length);
   };
 
   // Call iterator(value, key, collection) for each element of collection.
